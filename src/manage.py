5af2211fbd4d84f4afab94a6e3cbf7c1bf073401
Kim Stone <techmate@Mac-mini.localdomain>
2021-05-03 19:25:52 -0400

    added multiple settings modules

--- src/manage.py
+++ src/manage.py
@@ -3,10 +3,15 @@
 import os
 import sys
 
+from decouple import config
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.development')
+
+    if config('DJANGO_SETTINGS_MODULE'):
+        os.environ['DJANGO_SETTINGS_MODULE'] = config('DJANGO_SETTINGS_MODULE')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,3 +25,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+DJANGO_SETTINGS_MODULE="main.settings.development"
